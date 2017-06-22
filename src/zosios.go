ce6abf67e655509ff1bb1b35cd9173023ac25e26
Tjiho Lun <tom.darboux@eisti.fr>
2017-06-22 16:30:44 +0200

    AMELIORATION INDEX + PAGE GALERIE + DIVERS AUTRE

--- src/zosios.go
+++ src/zosios.go
@@ -8,11 +8,32 @@ import (
     "path/filepath"
     "io/ioutil"
 )
-type Page struct {
-    Title    string
-    Articles []string
+
+type DetailGalerie struct {
+    Titre    string
+    Fichiers []string
 }
+
 var router = mux.NewRouter()
+
+func main(){
+    //liste des images pour le slider
+    //files, _ := filepath.Glob("static/galerie/top");
+    
+
+    router.HandleFunc("/", index)
+    router.HandleFunc("/index.html", index)
+    router.HandleFunc("/galerie.html", galerie)
+    router.HandleFunc("/couleur.html", couleur)
+    router.HandleFunc("/galerie/{dossier}.html", detailGalerie)
+    router.HandleFunc("/licenses.html", licenses)
+
+    s1 := http.StripPrefix("/static/",http.FileServer(http.Dir("./static/")))
+    router.PathPrefix("/").Handler(s1)
+    http.Handle("/", router)
+    http.ListenAndServe(":12345", nil)
+}
+
 
 func Extend(slice []string, element string) []string {
     n := len(slice)
@@ -28,23 +49,36 @@ func Extend(slice []string, element string) []string {
     return slice
 }
 
-
-
-func main(){
-    //liste des images pour le slider
-    //files, _ := filepath.Glob("static/galerie/top");
+func licenses(response http.ResponseWriter, request *http.Request) {
     
-
-    router.HandleFunc("/", index)
-    router.HandleFunc("/galerie", galerie)
-    s1 := http.StripPrefix("/static/",http.FileServer(http.Dir("./static/")))
-    router.PathPrefix("/").Handler(s1)
-    http.Handle("/", router)
-    http.ListenAndServe(":12345", nil)
+    //colors := []string{"#27bfe7","#5227e7","#c927e7","#f16bcd","#e71e50","#e78027","#e7bd27","#27e763","#4baf6b"}
+    colors := []string{"#fef9d1","#79a50a","#fd966d","#1a8ffb","#6b59cd","#38d9da","#ff3299","#e7bd27","fb02fe"}
+    t := template.New("Label de ma template")
+    
+    t = template.Must(t.ParseFiles("pages/template.html", "pages/licenses.html"))
+    err := t.ExecuteTemplate(response, "page", colors)
+ 
+    if err != nil {
+        log.Fatalf("Template execution: %s", err)
+    }
 }
 
+func couleur(response http.ResponseWriter, request *http.Request) {
+    
+    //colors := []string{"#27bfe7","#5227e7","#c927e7","#f16bcd","#e71e50","#e78027","#e7bd27","#27e763","#4baf6b"}
+    colors := []string{"#fef9d1","#79a50a","#fd966d","#1a8ffb","#6b59cd","#38d9da","#ff3299","#e7bd27","fb02fe"}
+    t := template.New("Label de ma template")
+    
+    t = template.Must(t.ParseFiles("pages/template.html", "pages/couleur.html"))
+    err := t.ExecuteTemplate(response, "page", colors)
+ 
+    if err != nil {
+        log.Fatalf("Template execution: %s", err)
+    }
+}
+
+
 func galerie(response http.ResponseWriter, request *http.Request) {
-    //files, _ := filepath.Glob("static/galerie/*");
     files, _ := ioutil.ReadDir("./static/galerie")
     names_files := []string{}
     
@@ -63,6 +97,20 @@ func galerie(response http.ResponseWriter, request *http.Request) {
     }
 }
 
+func detailGalerie(response http.ResponseWriter, request *http.Request){
+    vars := mux.Vars(request)
+    files, _ := filepath.Glob("static/galerie/"+vars["dossier"]+"/*");
+
+    p := DetailGalerie{vars["dossier"], files}
+
+    t := template.New("Label de ma template")
+    t = template.Must(t.ParseFiles("pages/template.html", "pages/detailGalerie.html"))
+    err := t.ExecuteTemplate(response, "page", p)
+    
+    if err != nil {
+        log.Fatalf("Template execution: %s", err)
+    }
+}
 
 func index(response http.ResponseWriter, request *http.Request) {
     files, _ := filepath.Glob("static/galerie/top/*");
--- static/js/galerie.js
+++ static/js/galerie.js
@@ -10,7 +10,8 @@ function stringToInt(str)
 
 function coloration()
 {   
-    color = ["#27bfe7","#5227e7","#c927e7","#f16bcd","#e71e50","#e78027","#e7bd27","#27e763","#4baf6b"];
+    //color = ["#27bfe7","#5227e7","#c927e7","#f16bcd","#e71e50","#e78027","#e7bd27","#27e763","#4baf6b"];
+    color = ["#fef9d1","#79a50a","#fd966d","#1a8ffb","#6b59cd","#38d9da","#ff3299","#e7bd27","fb02fe"];
     for(var ele of document.getElementsByClassName("dossier"))
     {
         var str = ele.getElementsByTagName("h3")[0].innerText;
