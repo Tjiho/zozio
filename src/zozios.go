4a6b2212601b214d4c61a3f8a3f41ce3382fda89
Tjiho Lun <tom.darboux@eisti.fr>
2017-09-07 23:31:51 +0200

    slider + style acceuil

--- src/zozios.go
+++ src/zozios.go
@@ -1,379 +1,301 @@
 package main
 
 import (
-    "net/http"
-    "net/url"
+	"net/http"
+	"net/url"
+
 	"github.com/gorilla/mux"
-    //"github.com/gorilla/securecookie"
-    "html/template"
-    "log"
-    "io/ioutil"
-    "github.com/nfnt/resize"
-    "image/jpeg"
-    "os"
-    "fmt"
-    "path/filepath"
+	//"github.com/gorilla/securecookie"
+	"fmt"
+	"html/template"
+	"image/jpeg"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/nfnt/resize"
 )
 
 type Link struct {
-    Image   string
-    Link    string
+	Image string
+	Link  string
 }
 
 var router = mux.NewRouter()
 
-func main(){
-    //liste des images pour le slider
-    //files, _ := filepath.Glob("static/galerie/top");
-    
+func main() {
+	//liste des images pour le slider
+	//files, _ := filepath.Glob("static/galerie/top");
 
-    router.HandleFunc("/", categorie)
-    router.HandleFunc("/index.html", categorie)
-    router.HandleFunc("/galerie.html", galerie)
-    router.HandleFunc("/galerie/{dossier}.html", detailGalerie)
-    router.HandleFunc("/login.html", login)
-    router.HandleFunc("/miniature/{dossier}/{file}",miniature)
-    
-    s1 := http.StripPrefix("/static/",http.FileServer(http.Dir("./static/")))
-    router.PathPrefix("/").Handler(s1)
-    http.Handle("/", router)
-    http.ListenAndServe(":12345", nil)
+	router.HandleFunc("/", categorie)
+	router.HandleFunc("/index.html", index)
+	router.HandleFunc("/galerie.html", galerie)
+	router.HandleFunc("/galerie/{dossier}.html", detailGalerie)
+	router.HandleFunc("/login.html", login)
+	router.HandleFunc("/miniature/{dossier}/{file}", miniature)
+
+	s1 := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
+	router.PathPrefix("/").Handler(s1)
+	http.Handle("/", router)
+	http.ListenAndServe(":12345", nil)
 }
 
 func Extend(slice []string, element string) []string {
-    n := len(slice)
-    if n == cap(slice) {
-        // Slice is full; must grow.
-        // We double its size and add 1, so if the size is zero we still grow.
-        newSlice := make([]string, len(slice), 2*len(slice)+1)
-        copy(newSlice, slice)
-        slice = newSlice
-    }
-    slice = slice[0 : n+1]
-    slice[n] = element
-    return slice
+	n := len(slice)
+	if n == cap(slice) {
+		// Slice is full; must grow.
+		// We double its size and add 1, so if the size is zero we still grow.
+		newSlice := make([]string, len(slice), 2*len(slice)+1)
+		copy(newSlice, slice)
+		slice = newSlice
+	}
+	slice = slice[0 : n+1]
+	slice[n] = element
+	return slice
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 func miniature(response http.ResponseWriter, request *http.Request) {
-    
-    vars := mux.Vars(request);
-    pathMinDir := "static/galerie/"+vars["dossier"]+"/min/";
-    path := "static/galerie/"+vars["dossier"]+"/"+vars["file"];
-    pathMinFile := pathMinDir + vars["file"]; 
-    ok := true;
 
-    //get image
-        
-    file, err := os.Open(path);
+	vars := mux.Vars(request)
+	pathMinDir := "static/galerie/" + vars["dossier"] + "/min/"
+	path := "static/galerie/" + vars["dossier"] + "/" + vars["file"]
+	pathMinFile := pathMinDir + vars["file"]
+	ok := true
+
+	//get image
+
+	file, err := os.Open(path)
 	if err != nil {
-        log.Fatal("\n\roups:",err)
-        response.WriteHeader(http.StatusNotFound)
-        
-        ok = false
-    }
-     
-    //check if miniature dir exist    
-    stat, err := os.Stat(pathMinDir);
-    if err != nil {
-        if os.IsNotExist(err) {
-            fmt.Printf("new dir!")
-            os.Mkdir(pathMinDir,os.FileMode(0755))
-        } else {
-            fmt.Printf("error!")
-            response.WriteHeader(http.StatusInternalServerError)
-            ok = false
-        }
-    } else {
-        if(! stat.IsDir()){
-            fmt.Printf("error!")
-            response.WriteHeader(http.StatusInternalServerError)
-            ok = false
-        }
-    }
-    
+		log.Fatal("\n\roups:", err)
+		response.WriteHeader(http.StatusNotFound)
 
-    if(ok == true){
-        
-        //check if miniature file already exist
-        _, err := os.Stat(pathMinFile);
-        if err != nil {
-            if os.IsNotExist(err) {
-                print("go resize : "+path);
-        
-                // decode jpeg into image.Image
-                img, err := jpeg.Decode(file)
-                if err != nil {
-                    log.Fatal("\n\roups:",err)
-                }
-                file.Close()
-                
-                
-                // resize
-                m := resize.Thumbnail(300, 300, img, resize.Lanczos3)
-                
-                //create new image   
-                out, err := os.Create(pathMinFile);
-                if err != nil {
-                    log.Fatal(err)
-                }
-                defer out.Close()
-                jpeg.Encode(out, m, nil)
-            } else {
-                response.WriteHeader(http.StatusInternalServerError)
-            }
-        }
-        http.Redirect(response, request, "/"+pathMinFile, http.StatusTemporaryRedirect)
-    }
-    
-     
+		ok = false
+	}
+
+	//check if miniature dir exist
+	stat, err := os.Stat(pathMinDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("new dir!")
+			os.Mkdir(pathMinDir, os.FileMode(0755))
+		} else {
+			fmt.Printf("error!")
+			response.WriteHeader(http.StatusInternalServerError)
+			ok = false
+		}
+	} else {
+		if !stat.IsDir() {
+			fmt.Printf("error!")
+			response.WriteHeader(http.StatusInternalServerError)
+			ok = false
+		}
+	}
+
+	if ok == true {
+
+		//check if miniature file already exist
+		_, err := os.Stat(pathMinFile)
+		if err != nil {
+			if os.IsNotExist(err) {
+				print("go resize : " + path)
+
+				// decode jpeg into image.Image
+				img, err := jpeg.Decode(file)
+				if err != nil {
+					log.Fatal("\n\roups:", err)
+				}
+				file.Close()
+
+				// resize
+				m := resize.Thumbnail(300, 300, img, resize.Lanczos3)
+
+				//create new image
+				out, err := os.Create(pathMinFile)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer out.Close()
+				jpeg.Encode(out, m, nil)
+			} else {
+				response.WriteHeader(http.StatusInternalServerError)
+			}
+		}
+		http.Redirect(response, request, "/"+pathMinFile, http.StatusTemporaryRedirect)
+	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 func galerie(response http.ResponseWriter, request *http.Request) {
-    
-    files, _ := ioutil.ReadDir("./static/galerie")
-    names_files := []string{}
 
+	files, _ := ioutil.ReadDir("./static/galerie")
+	names_files := []string{}
 
-    links := []Link{
-        Link{
-            Image:  "/static/images/home.svg",
-            Link:   "/index.html",
-        },
-    }
+	links := []Link{
+		Link{
+			Image: "/static/images/home.svg",
+			Link:  "/index.html",
+		},
+	}
 
+	for _, f := range files {
+		names_files = Extend(names_files, f.Name())
+	}
 
-    for _, f := range files {
-            names_files = Extend(names_files,f.Name())            
-    }
-    
+	data := struct {
+		Files      []string
+		Links      []Link
+		Nav        bool
+		Content_id string
+	}{
+		names_files,
+		links,
+		true,
+		"galeries",
+	}
 
-    data := struct {
-        Files []string
-        Links []Link
-        Nav bool
-        Content_id string
-    } {
-        names_files,
-        links,
-        true,
-        "galeries",
-    }
-        
-    
-    //colors := []string{"#27bfe7","#5227e7","#c927e7","#f16bcd","#e71e50","#e78027","#e7bd27","#27e763","#4baf6b"}
-    t := template.New("")
-    
-    t = template.Must(t.ParseFiles("pages/template.html", "pages/galerie.html","pages/header-menu.html"))
-    err := t.ExecuteTemplate(response, "page", data)
- 
-    if err != nil {
-        log.Fatalf("Template execution: %s", err)
-    }
+	//colors := []string{"#27bfe7","#5227e7","#c927e7","#f16bcd","#e71e50","#e78027","#e7bd27","#27e763","#4baf6b"}
+	t := template.New("")
+
+	t = template.Must(t.ParseFiles("pages/template.html", "pages/galerie.html", "pages/header-menu.html"))
+	err := t.ExecuteTemplate(response, "page", data)
+
+	if err != nil {
+		log.Fatalf("Template execution: %s", err)
+	}
 }
 
+func detailGalerie(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	path := "static/galerie/" + vars["dossier"]
+	files, _ := ioutil.ReadDir(path)
+	names_files := []string{}
 
+	//list files
+	for _, f := range files {
+		var extension = filepath.Ext(f.Name())
+		if !f.IsDir() && (extension == ".jpg" || extension == ".JPG") {
+			names_files = Extend(names_files, vars["dossier"]+"/"+f.Name())
+		}
 
+	}
 
+	//create menu
+	links := []Link{
+		Link{
+			Image: "/static/images/retour.svg",
+			Link:  "/galerie.html",
+		},
+		Link{
+			Image: "/static/images/home.svg",
+			Link:  "/index.html",
+		},
+		Link{
+			Image: "/static/images/upload.svg",
+			Link:  "/index.html",
+		},
+	}
 
+	data := struct {
+		Files      []string
+		Title      string
+		Links      []Link
+		Nav        bool
+		Content_id string
+	}{
+		names_files,
+		vars["dossier"],
+		links,
+		true,
+		"photos",
+	}
 
+	t := template.New("")
+	t = template.Must(t.ParseFiles("pages/template.html", "pages/detailGalerie.html", "pages/header-menu.html"))
+	err := t.ExecuteTemplate(response, "page", data)
 
-
-
-
-
-
-
-
-func detailGalerie(response http.ResponseWriter, request *http.Request){
-    vars := mux.Vars(request)
-    path := "static/galerie/"+vars["dossier"];
-    files, _ := ioutil.ReadDir(path)
-    names_files := []string{}
-
-    //list files
-    for _, f := range files {
-        var extension = filepath.Ext(f.Name())
-        if(! f.IsDir() && (extension == ".jpg" || extension == ".JPG")) {
-            names_files = Extend(names_files,"miniature/"+vars["dossier"]+"/"+f.Name())            
-        }
-        
-    }
-
-    //create menu
-    links := []Link{
-        Link{
-            Image:  "/static/images/retour.svg",
-            Link:   "/galerie.html",
-        },
-        Link{
-            Image:  "/static/images/home.svg",
-            Link:   "/index.html",
-        },
-        Link{
-            Image:  "/static/images/upload.svg",
-            Link:   "/index.html",
-        },
-        
-    }
-
-    data := struct {
-        Files []string
-        Title string
-        Links []Link
-        Nav bool
-        Content_id string
-    } {
-        names_files,
-        vars["dossier"],
-        links,
-        true,
-        "photos",
-    }
-
-    t := template.New("")
-    t = template.Must(t.ParseFiles("pages/template.html", "pages/detailGalerie.html","pages/header-menu.html"))
-    err := t.ExecuteTemplate(response, "page", data)
-    
-    if err != nil {
-        log.Fatalf("Template execution: %s", err)
-    }
+	if err != nil {
+		log.Fatalf("Template execution: %s", err)
+	}
 }
-
-
-
-
-
-
-
-
-
-
 
 /*********** Acceuil **********/
 /******************************/
 func index(response http.ResponseWriter, request *http.Request) {
 
-    names_files := []string{}
+	names_files := []string{}
 
-    data := struct {
-        Files []string
-        Links []Link
-        Nav bool
-        Content_id string
-    } {
-        names_files,
-        []Link{},
-        false,
-        "index",
-    }
-    
-    t := template.New("")
-    t = template.Must(t.ParseFiles("pages/template.html", "pages/index.html","pages/header-title.html"))
-    err := t.ExecuteTemplate(response, "page", data)
- 
-    if err != nil {
-        log.Fatalf("Template execution: %s", err)
-    }
+	data := struct {
+		Files      []string
+		Links      []Link
+		Nav        bool
+		Content_id string
+	}{
+		names_files,
+		[]Link{},
+		false,
+		"index",
+	}
+
+	t := template.New("")
+	t = template.Must(t.ParseFiles("pages/template.html", "pages/index.html", "pages/header-title.html"))
+	err := t.ExecuteTemplate(response, "page", data)
+
+	if err != nil {
+		log.Fatalf("Template execution: %s", err)
+	}
 }
 
+func categorie(response http.ResponseWriter, request *http.Request) {
 
+	names_files := []string{}
 
+	data := struct {
+		Files      []string
+		Links      []Link
+		Nav        bool
+		Content_id string
+	}{
+		names_files,
+		[]Link{},
+		false,
+		"categories",
+	}
 
+	t := template.New("Label de ma template")
+	t = template.Must(t.ParseFiles("pages/template.html", "pages/categorie.html", "pages/header-title.html"))
+	err := t.ExecuteTemplate(response, "page", data)
 
-
-
-
-
-
-
-func categorie(response http.ResponseWriter, request *http.Request) {    
-
-    names_files := []string{}
-
-    data := struct {
-        Files []string
-        Links []Link
-        Nav bool
-        Content_id string
-    } {
-        names_files,
-        []Link{},
-        false,
-        "categories",
-    }
-    
-    t := template.New("Label de ma template")
-    t = template.Must(t.ParseFiles("pages/template.html", "pages/categorie.html","pages/header-title.html"))
-    err := t.ExecuteTemplate(response, "page", data)
- 
-    if err != nil {
-        log.Fatalf("Template execution: %s", err)
-    }
+	if err != nil {
+		log.Fatalf("Template execution: %s", err)
+	}
 }
-
-
-
-
-
-
-
-
-
-
 
 func login(response http.ResponseWriter, request *http.Request) {
-    //name := request.FormValue("login")
-    //pass := request.FormValue("password")
-    redirectTarget := "/"
-   
-        resp, err := http.PostForm( "http://localhost:8080/api/v1/users",
-                                    url.Values{ "name": {"toto"}, 
-                                                "surname": {"titi"},
-                                                "password":{"123456789123"},
-                                                "password_confirmation":{"1234567891"},
-                                                "pseudo":{"toto"}})
-        if err != nil {
-	        log.Fatalf("Template execution: %s", err)
-        }
-        defer resp.Body.Close()
-        body, err := ioutil.ReadAll(resp.Body)
-        log.Fatalf("Template execution: %s", body)
-        
-        
-        
-        //setSession(name, response)
-        redirectTarget = "/galerie"
-    
-    http.Redirect(response, request, redirectTarget, 302)
+	//name := request.FormValue("login")
+	//pass := request.FormValue("password")
+	redirectTarget := "/"
+
+	resp, err := http.PostForm("http://localhost:8080/api/v1/users",
+		url.Values{"name": {"toto"},
+			"surname":               {"titi"},
+			"password":              {"123456789123"},
+			"password_confirmation": {"1234567891"},
+			"pseudo":                {"toto"}})
+	if err != nil {
+		log.Fatalf("Template execution: %s", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	log.Fatalf("Template execution: %s", body)
+
+	//setSession(name, response)
+	redirectTarget = "/galerie"
+
+	http.Redirect(response, request, redirectTarget, 302)
 }
- 
+
 func logoutHandler(response http.ResponseWriter, request *http.Request) {
-    //clearSession(response)
-    http.Redirect(response, request, "/", 302)
+	//clearSession(response)
+	http.Redirect(response, request, "/", 302)
 }
---
+++ static/js/slider.js
@@ -0,0 +1,119 @@
+function cacheSlider()
+{
+    var pop = document.getElementById("pop");
+    pop.style.display = "none";//display slider
+}
+
+
+//init slider with image in arg
+function slider(image)
+{
+    var index = list_files.indexOf(image);
+
+    var pop = document.getElementById("pop");
+    pop.style.display = "flex";//display slider
+
+    pop.getElementsByClassName("after")[0].src = "/static/images/loader1.svg";
+    pop.getElementsByClassName("after")[0].onclick = null;
+    pop.getElementsByClassName("before")[0].src = "/static/images/loader1.svg";
+
+    var images = pop.getElementsByClassName("images")[0];
+    images.innerHTML = "";
+    
+    var img_dom = document.createElement("img");
+    
+    img_dom.className = "visible";
+    images.appendChild(img_dom);
+    img_dom.onload = function()
+    {
+        loadNext(index);
+        loadPrevious(index,img_dom);
+    }
+    img_dom.src = "/static/galerie/"+image;
+
+}
+
+function loadNext(i)
+{
+    console.log("next !");
+    if(i < list_files.length - 1)
+    {
+        var pop = document.getElementById("pop");
+        var images = pop.getElementsByClassName("images")[0];
+        var img_dom = document.createElement("img");
+        img_dom.onload = function()
+        {
+            pop.getElementsByClassName("after")[0].src = "/static/images/arrow-right.svg";
+            pop.getElementsByClassName("after")[0].onclick = function(){ next(i) };
+            console.log("ok next!");
+        }
+        img_dom.src = "/static/galerie/"+list_files[i+1];
+        images.appendChild(img_dom);
+    }
+}
+
+
+function loadPrevious(i,image)
+{
+    if(i > 0)
+    {
+        var pop = document.getElementById("pop");
+        var images = pop.getElementsByClassName("images")[0];
+        var img_dom = document.createElement("img");
+        img_dom.onload = function()
+        {
+            pop.getElementsByClassName("before")[0].src = "/static/images/arrow-left.svg";
+            pop.getElementsByClassName("before")[0].onclick = function(){ previous(i) };
+            console.log("ok before!");
+        }
+        img_dom.src = "/static/galerie/"+list_files[i-1];
+        images.insertBefore(img_dom,image);
+    }
+}
+
+function next(i)
+{
+    var pop = document.getElementById("pop");
+    var enCours = pop.getElementsByClassName("visible")[0];
+    enCours.classList.remove("visible");
+    var suivant = enCours.nextElementSibling;
+    if(suivant != null)
+    {
+        suivant.classList.add("visible");
+        if(suivant.nextElementSibling == null)
+        {
+            pop.getElementsByClassName("after")[0].src = "/static/images/loader1.svg";
+            pop.getElementsByClassName("after")[0].onclick = null;
+            loadNext(i+1);
+        }
+
+        if(suivant.previousElementSibling != null)
+        {
+            pop.getElementsByClassName("before")[0].src = "/static/images/arrow-left.svg";
+            pop.getElementsByClassName("before")[0].onclick = function(){ previous(i) };
+        }
+    }
+}
+
+function previous(i)
+{
+    var pop = document.getElementById("pop");
+    var enCours = pop.getElementsByClassName("visible")[0];
+    enCours.classList.remove("visible");
+    var precedent = enCours.previousElementSibling;
+    if(precedent != null)
+    {
+        precedent.classList.add("visible");
+        if(precedent.previousElementSibling == null)
+        {
+            pop.getElementsByClassName("before")[0].src = "/static/images/loader1.svg";
+            pop.getElementsByClassName("before")[0].onclick = null;
+            loadPrevious(i-1,precedent)
+        }
+        if(precedent.nextElementSibling != null)
+        {
+            pop.getElementsByClassName("after")[0].src = "/static/images/arrow-right.svg";
+            pop.getElementsByClassName("after")[0].onclick = function(){ next(i) };
+        }
+    }
+}
