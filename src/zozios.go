c5e776b2212fb93d38b9916f821e1bbc31078554
Tjiho Lun <tom.darboux@eisti.fr>
2017-10-29 01:35:30 +0200

    galery and images smaller on small devices

--- src/zozios.go
+++ src/zozios.go
@@ -30,7 +30,7 @@ func main() {
 	s1 := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
 	router.PathPrefix("/").Handler(s1)
 	http.Handle("/", router)
-	http.ListenAndServe(":12345", nil)
+	http.ListenAndServe("127.0.0.1:8764", nil)
 }
 
 func Extend(slice []string, element string) []string {
