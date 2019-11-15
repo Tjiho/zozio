936a9683b8361d5ab7baa36ef11e5d8267dfed4c
Tjiho Lun <tom.darboux@eisti.fr>
2019-11-15 04:50:47 +0100

    add new style to slider and improve navbar

--- static/js/slider.js
+++ static/js/slider.js
@@ -13,9 +13,9 @@ function slider(image)
     var pop = document.getElementById("pop");
     pop.style.display = "flex";//display slider
 
-    pop.getElementsByClassName("after")[0].src = "/static/images/loader1.svg";
+    //pop.getElementsByClassName("after")[0].src = "/static/images/loader1.svg";
     pop.getElementsByClassName("after")[0].onclick = null;
-    pop.getElementsByClassName("before")[0].src = "/static/images/loader1.svg";
+    //pop.getElementsByClassName("before")[0].src = "/static/images/loader1.svg";
 
     var images = pop.getElementsByClassName("images")[0];
     images.innerHTML = "";
@@ -43,8 +43,10 @@ function loadNext(i)
         var img_dom = document.createElement("img");
         img_dom.onload = function()
         {
-            pop.getElementsByClassName("after")[0].src = "/static/images/arrow-right.svg";
+            // pop.getElementsByClassName("after")[0].src = "/static/images/arrow-right.svg";
+        	pop.getElementsByClassName("after")[0].classList.remove("progress");
             pop.getElementsByClassName("after")[0].onclick = function(){ next(i) };
+    		img_dom.classList.add("next-img");
             console.log("ok next!");
         }
         img_dom.src = list_files[i+1];
@@ -62,8 +64,10 @@ function loadPrevious(i,image)
         var img_dom = document.createElement("img");
         img_dom.onload = function()
         {
-            pop.getElementsByClassName("before")[0].src = "/static/images/arrow-left.svg";
+            // pop.getElementsByClassName("before")[0].src = "/static/images/arrow-left.svg";
+        	pop.getElementsByClassName("before")[0].classList.remove("progress");
             pop.getElementsByClassName("before")[0].onclick = function(){ previous(i) };
+    		img_dom.classList.add("previous-img");
             console.log("ok before!");
         }
         img_dom.src = list_files[i-1];
@@ -76,22 +80,28 @@ function next(i)
     var pop = document.getElementById("pop");
     var enCours = pop.getElementsByClassName("visible")[0];
     enCours.classList.remove("visible");
+    enCours.classList.add("previous-img");
     var suivant = enCours.nextElementSibling;
+    var precedent = enCours.previousElementSibling;
     if(suivant != null)
     {
         suivant.classList.add("visible");
+    	suivant.classList.remove("next-img");
+    	if (precedent != null)
+			precedent.classList.remove("previous-img");
         if(suivant.nextElementSibling == null)
         {
-            pop.getElementsByClassName("after")[0].src = "/static/images/loader1.svg";
+            pop.getElementsByClassName("after")[0].classList.add("progress")
             pop.getElementsByClassName("after")[0].onclick = null;
             loadNext(i+1);
         }
         else
         {
             pop.getElementsByClassName("after")[0].onclick = function(){ next(i+1) };
-        }
+        	suivant.nextElementSibling.classList.add("next-img")
+		}
         
-        pop.getElementsByClassName("before")[0].src = "/static/images/arrow-left.svg";
+        pop.getElementsByClassName("before")[0].classList.remove("progress");
         pop.getElementsByClassName("before")[0].onclick = function(){ previous(i+1) };
         
     }
@@ -102,22 +112,31 @@ function previous(i)
     var pop = document.getElementById("pop");
     var enCours = pop.getElementsByClassName("visible")[0];
     enCours.classList.remove("visible");
+    enCours.classList.add("next-img");
+    
     var precedent = enCours.previousElementSibling;
+    var suivant = enCours.nextElementSibling;
     if(precedent != null)
     {
+
+    	precedent.classList.remove("previous-img");
         precedent.classList.add("visible");
+    	if (suivant != null)
+			suivant.classList.remove("next-img");
         if(precedent.previousElementSibling == null)
         {
-            pop.getElementsByClassName("before")[0].src = "/static/images/loader1.svg";
+            // pop.getElementsByClassName("before")[0].src = "/static/images/loader1.svg";
             pop.getElementsByClassName("before")[0].onclick = null;
             loadPrevious(i-1,precedent)
         }
         else
         {
             pop.getElementsByClassName("before")[0].onclick = function(){ previous(i-1) };
-        }
+        	precedent.previousElementSibling.classList.add("previous-img");
+
+		}
         
-        pop.getElementsByClassName("after")[0].src = "/static/images/arrow-right.svg";
+        // pop.getElementsByClassName("after")[0].src = "/static/images/arrow-right.svg";
         pop.getElementsByClassName("after")[0].onclick = function(){ next(i-1) };
         
     }
